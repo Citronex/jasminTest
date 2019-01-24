c646d208d7045aae42c123fbc4f2a601ceae0750
Francisco J Mejias <francisco.mejias@booker.com>
2019-01-23 22:22:13 -0500

    finished section on js testing

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -1,9 +1,35 @@
 describe('Address Book', function() {
-    it('should be able to add a contact', function() {
-        let addressBook = new AddressBook();
-        let thisContact = new Contact();
+    let addressBook;
+    let thisContact;
 
+    beforeEach(function() {
+        addressBook = new AddressBook();
+        thisContact = new Contact();
+    });
+
+    it('should be able to add a contact', function() {
         addressBook.addContact(thisContact);
         expect(addressBook.getContact(0)).toBe(thisContact);
     });
+    it('should be able to delete a contact', function(){
+        addressBook.deleteContact(0);
+        expect(addressBook.getContact(0)).not.toBeDefined();
+    });
 });
+
+describe('Async Address Book', function(){
+    let addressBook = new AddressBook();
+
+    beforeEach(function(done) {
+        addressBook.getInitialContacts(function(){
+            done();
+        })
+    });
+
+    it('should grab initial contacts', function(done) {
+        addressBook = new AddressBook();
+
+        addressBook.getInitialContacts();
+        expect(addressBook.initialComplete).toBe(true);
+    });
+})
--- src/AddressBook.js
+++ src/AddressBook.js
@@ -1,13 +1,31 @@
 class AddressBook {
+
     constructor(contacts = []){
         this.contacts = contacts;
+        this.initialComplete = false;
+    };
+
+    getContact(position) {
+        return this.contacts[position];
+    };
+
+    addContact(contact){
+        this.contacts.push(contact);
+    };
+
+    deleteContact(position){
+        this.contacts.splice(position, 1);
     }
 
-getContact(position) {
-    return this.contacts[position];
-}
+    getInitialContacts(cb) {
+        let self = this;
 
-addContact(contact){
-    this.contacts.push(contact);
-}
+        setTimeout(function(){
+            self.initialComplete = true;
+            if(cb) {
+                return cb();
+            }
+        }, 3)
+    }
+
 }
